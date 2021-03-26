178a1b66d6689d37a82fb38dc769ecf553db8159
FanruiC <joker@tju.edu.cn>
2021-03-27 01:27:10 +1100

    variable declariation

---
+++ basicCode/math.cpp
@@ -0,0 +1,18 @@
+// practice about math in c++
+#include<iostream>
+using namespace std;
+
+int  main()
+{
+	double a;
+	float b;
+	cout << "now we are gonna add a and b" << endl;
+	cout << "please write a value for a";
+	cin >> a;
+	cout << "please state a value for b";
+	cin >> b;
+	cout << a+b << endl;
+	cout << 9*9 << endl;
+	return 0;
+}
+
