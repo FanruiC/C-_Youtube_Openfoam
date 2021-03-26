b366a7287cd3a58c39d301f5ce63861617d10c87
FanruiC <joker@tju.edu.cn>
2021-03-27 00:48:38 +1100

    helloworld

---
+++ basicCode/hello_world.cpp
@@ -0,0 +1,11 @@
+// A hello world program
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	cout << "I am a big openfoam fan!!!" << endl;
+	return 0;
+}
+
